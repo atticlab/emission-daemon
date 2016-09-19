1401c4b22e402d1a5909bb932b94525a89b03b42
ihor <for0work0@gmail.com>
2016-09-19 20:57:36 +0300

    SMAR-202: Change dev.stellar to stellar in config

--- app/config.js
+++ app/config.js
@@ -14,7 +14,7 @@ config.base_currency = 'UAH';
 
 
 // Timeout between requests to oracle (in ms)
-config.horizon_url       = 'http://dev.stellar.attic.pw:8010';
+config.horizon_url       = 'http://stellar.attic.pw:8000';
 config.bank_public_key   = 'GAWIB7ETYGSWULO4VB7D6S42YLPGIC7TY7Y2SSJKVOTMQXV5TILYWBUA';
 config.emission_key_hash = 'eyJpdiI6IjFLVTNjeHFpM0NDa2xpNEc3UG9DWVE9PSIsInYiOjEsIml0ZXIiOjEwMDAsImtzIjoxMjgsInRzIjo2NCwibW9kZSI6ImNjbSIsImFkYXRhIjoiIiwiY2lwaGVyIjoiYWVzIiwic2FsdCI6IllZR3JhbjQyUzZnPSIsImN0IjoiSDV4MnRDZHdwZ2NMTmxPZHNzVEtBRUlNNDB5dVFtWHZQdGR2Y2t0QWc5dDNUSUV1YUtmcnZEclpaUlRpRG1pNFJ4ZEtmZnZkNFlBbm82ZU5wUjQxK3c9PSJ9';
 
