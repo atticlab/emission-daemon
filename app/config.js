73bec3327abbdaa66746985923b823f25f5a9e0a
ihor <for0work0@gmail.com>
2016-09-19 21:21:09 +0300

    SMAR-202: Change dev.stellar to stellar in config - change bank public key and hash

--- app/config.js
+++ app/config.js
@@ -15,7 +15,7 @@ config.base_currency = 'UAH';
 
 // Timeout between requests to oracle (in ms)
 config.horizon_url       = 'http://stellar.attic.pw:8000';
-config.bank_public_key   = 'GAWIB7ETYGSWULO4VB7D6S42YLPGIC7TY7Y2SSJKVOTMQXV5TILYWBUA';
-config.emission_key_hash = 'eyJpdiI6IjFLVTNjeHFpM0NDa2xpNEc3UG9DWVE9PSIsInYiOjEsIml0ZXIiOjEwMDAsImtzIjoxMjgsInRzIjo2NCwibW9kZSI6ImNjbSIsImFkYXRhIjoiIiwiY2lwaGVyIjoiYWVzIiwic2FsdCI6IllZR3JhbjQyUzZnPSIsImN0IjoiSDV4MnRDZHdwZ2NMTmxPZHNzVEtBRUlNNDB5dVFtWHZQdGR2Y2t0QWc5dDNUSUV1YUtmcnZEclpaUlRpRG1pNFJ4ZEtmZnZkNFlBbm82ZU5wUjQxK3c9PSJ9';
+config.bank_public_key   = 'GCGN6JATSN2ZEWGNWNCMIWKOY3YFYD2UNRGE4ZRFQCMJ5XRBSYHQZI2X';
+config.emission_key_hash = 'eyJpdiI6Iktmbm1TaGMvbzRQNE5JcDJCZ2ZSS3c9PSIsInYiOjEsIml0ZXIiOjEwMDAsImtzIjoxMjgsInRzIjo2NCwibW9kZSI6ImNjbSIsImFkYXRhIjoiIiwiY2lwaGVyIjoiYWVzIiwic2FsdCI6Ikk4S1JhNERPV1lNPSIsImN0IjoiZ3pOUW5WamNSUWR0REtyNytPOFo3cVVEQ0NZNWU4WDEwSzZqMS9DOU5EOVdxY3dQRXlFcmJYbjZJaFlEZGJ2UmM4R0F2eEJkTjFRbi9WcFo5WThJTXc9PSJ9';
 
 module.exports = config;
