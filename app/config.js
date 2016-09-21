a910a7c187719cac3d7e9086011e8dff2e936e8f
ihor <for0work0@gmail.com>
2016-09-21 13:40:32 +0300

    change stellar to blockchain.smartmoney.com.ua:80

--- app/config.js
+++ app/config.js
@@ -12,7 +12,7 @@ config.auth = {
 // Base currency
 config.base_currency = 'UAH';
 
-config.horizon_url       = 'http://stellar.attic.pw:8000';
+config.horizon_url       = 'http://blockchain.smartmoney.com.ua:80';
 config.master_public_key = 'GAWIB7ETYGSWULO4VB7D6S42YLPGIC7TY7Y2SSJKVOTMQXV5TILYWBUA';
 config.emission_key_hash = 'eyJpdiI6Iktmbm1TaGMvbzRQNE5JcDJCZ2ZSS3c9PSIsInYiOjEsIml0ZXIiOjEwMDAsImtzIjoxMjgsInRzIjo2NCwibW9kZSI6ImNjbSIsImFkYXRhIjoiIiwiY2lwaGVyIjoiYWVzIiwic2FsdCI6Ikk4S1JhNERPV1lNPSIsImN0IjoiZ3pOUW5WamNSUWR0REtyNytPOFo3cVVEQ0NZNWU4WDEwSzZqMS9DOU5EOVdxY3dQRXlFcmJYbjZJaFlEZGJ2UmM4R0F2eEJkTjFRbi9WcFo5WThJTXc9PSJ9';
 
